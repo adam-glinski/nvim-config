619040c8fda461501c33a1722b8cc2c986202de9
Adam Glinski <adam.s.glinki@gmail.com>
2024-05-09 15:37:43 +0200

    rookie mistake

--- lua/adamglinski/env.lua
+++ lua/adamglinski/env.lua
@@ -1,10 +1,10 @@
-local os = vim.loop.os_uname().sysname
+local osname = vim.loop.os_uname().sysname
 HOME = os.getenv("HOME")
 
-if(os == "Darwin") then
+if(osname == "Darwin") then
     NVIM_DIR = HOME .. ".config/nvim"
     NVIM_DATA_DIR =  "~/.local/share/nvim/"
-elseif os == "Windows_NT" then
+elseif osname == "Windows_NT" then
     NVIM_DIR = os.getenv("LOCALAPPDATA") .. "\\nvim"
     NVIM_DATA_DIR = os.getenv("LOCALAPPDATA") .. "\\nvim-data"
 end
