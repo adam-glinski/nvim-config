1e6b7e91c215a67c969746cb74eaeda1420f816c
Adam Glinski <adam.s.glinki@gmail.com>
2024-05-17 17:44:57 +0200

    Integrated obsidian.nvim and disabled neorg Since aint using it anymore

--- lua/adamglinski/plugin_config/neorg.lua
+++ lua/adamglinski/plugin_config/neorg.lua
@@ -1,23 +1,25 @@
-return {
-    "nvim-neorg/neorg",
-    dependencies = {"luarocks.nvim"},
-    version = "*",
-    lazy = false,
-    -- event = "BufEnter",
-    config = function ()
-        require('neorg').setup {
-            load = {
-                ["core.defaults"] = {}, -- Loads default behaviour
-                ["core.concealer"] = {}, -- Adds pretty icons to your documents
-                ["core.dirman"] = { -- Manages Neorg workspaces
-                    config = {
-                        workspaces = {
-                            notes = "~/notes",
-                            studia = "E:\\!Studia\\pre_notatki",
-                        },
-                    },
-                },
-        }
-    }
-    end
-}
+-- Aint using for now
+-- return {
+--     "nvim-neorg/neorg",
+--     dependencies = {"luarocks.nvim"},
+--     version = "*",
+--     lazy = false,
+--     -- event = "BufEnter",
+--     config = function ()
+--         require('neorg').setup {
+--             load = {
+--                 ["core.defaults"] = {}, -- Loads default behaviour
+--                 ["core.concealer"] = {}, -- Adds pretty icons to your documents
+--                 ["core.dirman"] = { -- Manages Neorg workspaces
+--                     config = {
+--                         workspaces = {
+--                             notes = "~/notes",
+--                             studia = "E:\\!Studia\\pre_notatki",
+--                         },
+--                     },
+--                 },
+--         }
+--     }
+--     end
+-- }
+return {}
