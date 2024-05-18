73bf39339a799f60bb4575b2115fe0a9902e9b7d
Adam Glinski <adam.s.glinki@gmail.com>
2024-05-18 23:54:51 +0200

    Refactored lsp cfg. Added automatic lsp server configuration. Added to cmp: buffer, path, cmdline

--- lua/adamglinski/plugin_config/lsp.lua
+++ lua/adamglinski/plugin_config/lsp.lua
@@ -1,4 +1,31 @@
--- TODO: get rid of depracated function calls
+local kind_icons = {
+  Text = "",
+  Method = "󰆧",
+  Function = "󰊕",
+  Constructor = "",
+  Field = "󰇽",
+  Variable = "󰂡",
+  Class = "󰠱",
+  Interface = "",
+  Module = "",
+  Property = "󰜢",
+  Unit = "",
+  Value = "󰎠",
+  Enum = "",
+  Keyword = "󰌋",
+  Snippet = "",
+  Color = "󰏘",
+  File = "󰈙",
+  Reference = "",
+  Folder = "󰉋",
+  EnumMember = "",
+  Constant = "󰏿",
+  Struct = "",
+  Event = "",
+  Operator = "󰆕",
+  TypeParameter = "󰅲",
+}
+
 return {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
@@ -11,42 +38,34 @@ return {
         -- Autocompletion
         { 'hrsh7th/nvim-cmp' },           -- Required
         { 'hrsh7th/cmp-nvim-lsp' },       -- Required
-        { 'hrsh7th/cmp-buffer' },         -- Attemtion to fix cmp
+        { 'hrsh7th/cmp-buffer' },         -- Optional
+        { 'hrsh7th/cmp-path' },         -- Optional
+        { 'hrsh7th/cmp-cmdline' },         -- Optional
         { 'L3MON4D3/LuaSnip' },           -- Required
         { 'rafamadriz/friendly-snippets' }, -- Optional (Added by me)
         { 'folke/neodev.nvim' },
     },
     config = function ()
+        local lsp = require('lsp-zero')
+        local lspconfig = require('lspconfig')
         require("neodev").setup()
         require("mason").setup({})
         require("mason-lspconfig").setup({
-            ensure_installed = {
-                'rust_analyzer',
-                'lua_ls',
-                'clangd',
+            handlers = {
+                function (server_name)
+                    lspconfig[server_name].setup({}) -- Automatic lsp server setup
+                end,
+                clangd = function ()
+                    lspconfig.clangd.setup({
+                        cmd = {"clangd"},
+                        single_file_support=true,
+                        -- cmd = {"clangd", "--fallback-style='{BasedOnStyle: LLVM, IndentWidth: 4}'"},
+                    })
+                end
             },
-        })
-
-        local lsp = require('lsp-zero')
-        lsp.preset('recommended')
-        -- lsp.ensure_installed({
-        --     'rust_analyzer',
-        --     'lua_ls',
-        --     'clangd',
-        -- })
-
-
-        -- TODO: Customize completion mapping
-        local cmp = require("cmp")
-        local lspconfig = require('lspconfig')
-        local cmp_select = {behavior = cmp.SelectBehavior.Select}
-        local cmp_mappings = lsp.defaults.cmp_mappings({
-            ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-            ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-            -- ['<CR>'] = cmp.mapping.confirm({select = true, behavior = cmp.ConfirmBehavior.Insert}),
-            -- ['<C-y>'] = cmp.mapping.confirm({select = true, behavior = cmp.ConfirmBehavior.Insert}),
-            ['<C-y>'] = cmp.mapping.confirm({select = true}),
-            ['<C-Space>'] = cmp.mapping.complete(),
+            ensure_installed = {
+                'lua_ls',
+            },
         })
 
         -- lsp.set_sign_icons({
@@ -63,9 +82,7 @@ return {
             info = ''
         })
 
-
-        -- TODO: Document mappings
-        lsp.on_attach(function(_, bufnr)
+        lsp.on_attach(function(client, bufnr)
             vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, {buffer = bufnr, remap = false, desc = "Go to definition"})
             vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, {buffer = bufnr, remap = false, desc = "Hover information"})
             vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, {buffer = bufnr, remap = false, desc = "Goto next diagnostic"})
@@ -79,40 +96,65 @@ return {
             vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format({formatting_options = { tabSize = 4 }}) end, {buffer = bufnr, remap = false, desc = "Format buffer"})
 
             vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, {buffer = bufnr, remap = false, desc = "Signature help"})
-            vim.keymap.set({"i", "n"}, "<C-t>", "<cmd>ClangdSwitchSourceHeader<cr>", {buffer = bufnr, remap = false, desc = "Switch between source/header"}) -- TODO: Enable only when clangd lsp is loaded
-            vim.keymap.set("n", "<leader>li", function () vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end, {remap = false, desc = "Inlay hints"})
+            if client.name == "clangd" then
+                vim.keymap.set({"i", "n"}, "<C-t>", "<cmd>ClangdSwitchSourceHeader<cr>", {buffer = bufnr, remap = false, desc = "Switch between source/header"})
+            end
+            vim.keymap.set("n", "<leader>li", function () vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({})) end, {remap = false, desc = "Inlay hints"})
         end)
 
-        lspconfig.lua_ls.setup({})
-        lspconfig.clangd.setup({
-            -- --style="{BasedOnStyle: llvm, IndentWidth: 4}"
-            -- cmd = {"clangd", "--fallback-style='{BasedOnStyle: LLVM, IndentWidth: 4}'"},
-            cmd = {"clangd"},
-            single_file_support=true,
+        local cmp = require("cmp")
+        local cmp_action = require("lsp-zero").cmp_action()
+        local cmp_select = {behavior = cmp.SelectBehavior.Select}
+        cmp.setup.cmdline({'/', '?'}, {
+            mapping = cmp.mapping.preset.cmdline(),
+            view = {
+                entries = {name = 'wildmenu', separator = '|' },
+            },
+            sources = {
+                { name = "buffer" }
+            }
         })
-        lspconfig.cmake.setup({})
-        lspconfig.bashls.setup({})
-
-
-        lsp.setup()
-
-
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            view = {
+                entries = {name = 'wildmenu', separator = '|' },
+            },
+            sources = {
+                {name = 'path'},
+                { name = 'cmdline' },
+            },
+            matching = { disallow_symbol_nonprefix_matching = false }
+        })
         cmp.setup({
-            formatting = lsp.cmp_format(),
-            mapping = cmp_mappings,
-            preselect = 'item',
+            mapping = {
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                -- ['<CR>'] = cmp.mapping.confirm({select = true}),
+                ['<C-y>'] = cmp.mapping.confirm({select = true}),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+                ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+                ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-d>'] = cmp.mapping.scroll_docs(4),
+            },
+            snippet = {
+                expand = function (args)
+                    require("luasnip").lsp_expand(args.body)
+                end
+            },
+            formatting = {
+                format = function (_, vim_item)
+                    vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
+                    return vim_item
+                end,
+                expandable_indicator = true,
+            },
             sources = {
                 { name = "nvim_lsp" },
-                { name = "orgmode" },
+                { name = "path" },
                 { name = "buffer" },
-                -- { name = "clangd" },
-                -- { name = "" },
-            completion = {
-                completeopt = 'menu,menuone,noinsert'
-            }
+                -- { name = "orgmode" },
             },
-
         })
     end
-
 }
