539ab843f11985bb207f4b72e4175517bb03f7f0
Adam Glinski <anonymuspl.yt@gmail.com>
2025-08-24 21:54:29 +0200

    Disabled obsidian plugins (unused rn), added render-md to lsp completion

--- lua/adamglinski/plugin_config/obsidian.lua
+++ lua/adamglinski/plugin_config/obsidian.lua
@@ -1,33 +1,34 @@
-return {
-    "epwalsh/obsidian.nvim",
-    version = "*",  -- recommended, use latest release instead of latest commit
-    lazy = true,
-    ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --       -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --       -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-    --       "BufReadPre" .. vim.fn.expand "~" .. "~/Documents/Studia/Notatki/**.md",
-    --       "BufNewFile" .. vim.fn.expand "~" .. "~/Documents/Studia/Notatki/**.md",
-    --       -- "BufReadPre '/Users/adam/Library/Mobile Documents/iCloud~md~obsidian/Documents'/Notatki/**.md",
-    --       -- "BufNewFile '/Users/adam/Library/Mobile Documents/iCloud~md~obsidian/Documents'/Notatki/**.md",
-    --     },
-        dependencies = {
-            -- Required.
-            "nvim-lua/plenary.nvim",
-
-            -- see below for full list of optional dependencies 👇
-        },
-        opts = {
-            workspaces = {
-                {
-                    name = "studia",
-                    path = "~/Documents/Studia/Notatki",
-                },
-                -- {
-                --     name = "work",
-                --     path = "~/vaults/work",
-                -- },
-            },
-        },
-    }
+-- return {
+--     "epwalsh/obsidian.nvim",
+--     version = "*",  -- recommended, use latest release instead of latest commit
+--     lazy = true,
+--     ft = "markdown",
+--     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+--     -- event = {
+--     --       -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+--     --       -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+--     --       "BufReadPre" .. vim.fn.expand "~" .. "~/Documents/Studia/Notatki/**.md",
+--     --       "BufNewFile" .. vim.fn.expand "~" .. "~/Documents/Studia/Notatki/**.md",
+--     --       -- "BufReadPre '/Users/adam/Library/Mobile Documents/iCloud~md~obsidian/Documents'/Notatki/**.md",
+--     --       -- "BufNewFile '/Users/adam/Library/Mobile Documents/iCloud~md~obsidian/Documents'/Notatki/**.md",
+--     --     },
+--         dependencies = {
+--             -- Required.
+--             "nvim-lua/plenary.nvim",
+--
+--             -- see below for full list of optional dependencies 👇
+--         },
+--         opts = {
+--             workspaces = {
+--                 {
+--                     name = "studia",
+--                     path = "~/Documents/Studia/Notatki",
+--                 },
+--                 -- {
+--                 --     name = "work",
+--                 --     path = "~/vaults/work",
+--                 -- },
+--             },
+--         },
+--     }
+return {}
